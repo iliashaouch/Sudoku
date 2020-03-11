ebe1cf752af019b8ef2c92ae0d9c353150957114
Mang'il <langlois.clement@gmail.com>
2020-03-10 21:50:35 -0400

    plus de sudou par defaut

--- TP2-Sudoku/TP2-Sudoku/Form1.cs
+++ TP2-Sudoku/TP2-Sudoku/Form1.cs
@@ -55,9 +55,21 @@ namespace TP2_Sudoku
                 { 0, 4, 0, 0, 0, 0, 0, 0, 7 },
                 { 0, 0, 7, 0, 0, 0, 3, 0, 0 }
             };
+            int[,] grid3 = {
+                { 0, 0, 0, 4, 0, 3, 0, 1, 0 },
+                { 0, 0, 6, 0, 0, 0, 0, 0, 7 },
+                { 0, 7, 0, 2, 5, 0, 0, 0, 0 },
+                { 9, 0, 1, 0, 0, 8, 0, 4, 6 },
+                { 2, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 5, 0, 0, 0, 0, 0, 3, 0 },
+                { 0, 0, 8, 0, 3, 0, 0, 0, 0 },
+                { 0, 0, 0, 1, 0, 0, 0, 7, 0 },
+                { 0, 4, 0, 0, 0, 0, 8, 0, 0 }
+            };
             listgrille.Add(grid0);
             listgrille.Add(grid1);
             listgrille.Add(grid2);
+            listgrille.Add(grid3);
             this.Load += new EventHandler(FormLoad);
         }
 
